5fb8cf518283e3d21e317c349cc2a03999acafd4
Iliyan-Y <maximizebg@gmail.com>
2020-06-16 21:04:30 +0100

    changes

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
   //redirect to the build folder
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
