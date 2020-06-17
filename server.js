f72a32469a95648f939adb6094c0f70242dba271
Iliyan-Y <maximizebg@gmail.com>
2020-06-17 10:15:22 +0100

    Deploy day2.0

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
 
   //redirect to the build folder
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
